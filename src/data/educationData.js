8787e981e96775a7d244f7fb33dce11d0d75e6cd
Andres <andres.torressandoval@hotmail.com>
2025-08-04 12:46:16 -0500

    Update info

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,20 +1,27 @@
 export const educationData = [
-    {
+        {
         id: 1,
+        institution: 'Universidad Jorge Tadeo Lozano',
+        course: 'Fullstack web development',
+        startYear: '2025',
+        endYear: '2025'
+    },
+    {
+        id: 2,
         institution: 'SoyHenry',
         course: 'Fullstack web development',
         startYear: '2023',
         endYear: '2023'
     },
     {
-        id: 2,
+        id: 3,
         institution: 'Coderhouse',
         course: 'Fullstack web development',
         startYear: '2022',
         endYear: '2022'
     },
     {
-        id: 3,
+        id: 4,
         institution: 'Universidad Politécnico Grancolombiano',
         course: '(BBA) Bachelor of Business Management',
         startYear: '2016',
--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,34 +1,41 @@
 export const experienceData = [
-    {
+        {
         id: 1,
+        company: 'Publicis Global Delivery',
+        jobtitle: 'Experience Technology Engineer',
+        startYear: 'Jul 2025',
+        endYear: 'Present'
+    },
+    {
+        id: 2,
         company: 'Publicis Global Delivery',
         jobtitle: 'Junior Experience Technology Engineer',
         startYear: 'Nov 2023',
         endYear: 'Present'
     },
     {
-        id: 2,
+        id: 3,
         company: 'Ambvalent',
         jobtitle: 'Fullstack Developer (freelance)',
         startYear: 'May 2023',
         endYear: 'Present'
     },
     {
-        id: 3,
+        id: 4,
         company: 'AMNphone',
         jobtitle: 'Fullstack developer (freelance)',
         startYear: 'Oct 2022',
         endYear: 'Apr 2023'
     },
     {
-        id: 4,
+        id: 5,
         company: 'Aruna Asesores SAS',
         jobtitle: 'Corporate website development and maintenance',
         startYear: 'Oct 2021',
         endYear: 'Feb 2023'
     },
     {
-        id: 5,
+        id: 6,
         company: 'Aruna Asesores SAS',
         jobtitle: 'Head of administrative division and quality',
         startYear: 'Jan 2020',
